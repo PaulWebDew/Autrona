0fe0c79c331ebe141f54e53e3f05ec43fe982f23
PaulWebDew <paulwebdew@gmail.com>
2022-12-12 14:13:51 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+// -----------------------slider--------------------------
+var swiper = new Swiper(".mySwiper", {
+   slidesPerView: 1,
+   speed: 800,
+   spaceBetween: 1400,
+   loop: true,
+   centeredSlides: true,
+   autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+   },
+   pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+   },
+   navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+   },
+});
