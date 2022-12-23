c17fe6727ba9f876c811715c345d59095ee36c0a
PaulWebDew <paulwebdew@gmail.com>
2022-12-23 17:40:57 +0200

    added languages

---
+++ lang.js
@@ -0,0 +1,46 @@
+const langArr = {
+
+
+   "services": {
+      "lt": "paslaugos",
+      "en": "services",
+      "ru": "услуги"
+   },
+   "about": {
+      "lt": "apie mus",
+      "en": "about",
+      "ru": "o нас"
+   },
+   "employement": {
+      "lt": "užimtumas",
+      "en": "enployement",
+      "ru": "соискателям"
+   },
+   "contacts": {
+      "lt": "kontaktai",
+      "en": "contacts",
+      "ru": "контакты"
+   },
+   "sp1": {
+      "lt": "JŪSŲ PARTNERIS",
+      "en": "YOUR PARTNER FOR",
+      "ru": "ВАШ ПАРТНЕР ДЛЯ"
+   },
+   "sp2": {
+      "lt": "PAPRASTAM",
+      "en": "SIMPLISITY",
+      "ru": "УДОБСТВА"
+   },
+   "sp3": {
+      "lt": "TRANSPORTAVIMUI",
+      "en": "TRANSPORTATION",
+      "ru": "ГРУЗОПЕРЕВОЗОК"
+   },
+
+   "days": {
+      "lt": "Pir-Pen",
+      "en": "Mon-Fri",
+      "ru": "Пн-Пт"
+   }
+
+}
